5308fa40574362d71d339c26d7cb0520176a93bf
Elijah Samuels <elijahsamuels@gmail.com>
2021-02-19 13:58:02 -0700

    commit tree 2ebda87c7427925a4625cdd00413d1f8a1687e4e

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Your code goes here
+
+// document.addEventListener("DOMContentLoaded", function() {
+// 	console.log("The DOM has loaded");
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+	contentChange()
+	// console.log("The DOM has loaded");
+});
+
+// console.log(
+// 	"This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+//   );
+  
+function contentChange() {
+	// document.getElementById("text").innerHTML = ("This is really cool!");
+	let a = document.getElementById("text")
+	a.innerHTML = ("This is really cool!");
+	// replace.innerHTML("This is really cool!");
+	};
+
